1b01d0bc8452f93ffc67e4c6c7cf0085f05a13fe
FrenchSolex <le.cowboy.fringant@hotmail.fr>
2025-11-04 11:31:15 +0100

    justification texte

--- app_pages/car_pollution.py
+++ app_pages/car_pollution.py
@@ -9,19 +9,18 @@ def render():
     st.markdown("---")
 
     st.subheader("Contexte du projet")
-    md_justify("""
-    Ce projet à pour objectif d'étudier les émissions des véhicules légers (VL) en France, en se basant sur un dataset fourni par l'ademe et préparé auparavant.
-    Le dataset fourni des informations sur les caractéristiques des véhicules ainsi que leurs émissions à différentes vitesses et lors d'un essai routier.
-    C'est ce que nous allons utiliser pour analyser le potentiel de réchauffement global (PGR) des véhicules par motorisation. 
+    md_justify("Ce projet à pour objectif d'étudier les émissions des véhicules légers (VL) en France, en se basant sur un dataset fourni par l'ademe et préparé auparavant." \
+    "Le dataset fourni des informations sur les caractéristiques des véhicules ainsi que leurs émissions à différentes vitesses et lors d'un essai routier." \
+    "C'est ce que nous allons utiliser pour analyser le potentiel de réchauffement global (PGR) des véhicules par motorisation." \
+    "Il est à noter que cet ensemble de données ne couvre pas tous les aspects des émissions des véhicules (uniquement ceux mesurés et produits par la motorisation)." 
+    )
 
-    Il est à noter que cet ensemble de données ne couvre pas tous les aspects des émissions des véhicules (uniquement ceux mesurés et produits par la motorisation). 
-    """)
-    md_justify("Données brutes accessibles ici : https://www.data.gouv.fr/fr/datasets/r/3b3cce6b-073d-4b4c-a68c-2744c92f4045")
+    st.markdown("Données brutes accessibles ici : https://www.data.gouv.fr/fr/datasets/r/3b3cce6b-073d-4b4c-a68c-2744c92f4045")
 
     md_justify("Pour pouvoir comparer les émissions, nous allons nous référer à quelques notions clés : ")
-    md_justify("Les gazs à effets de serres : https://jancovici.com/changement-climatique/gaz-a-effet-de-serre-et-cycle-du-carbone/quels-sont-les-gaz-a-effet-de-serre-quels-sont-leurs-contribution-a-leffet-de-serre/)")
-    md_justify("Les Hydrocarbures : https://www.geo.fr/environnement/hydrocarbure-definition-classification-et-utilisation-193625")
-    md_justify("Les polluants de l'air : https://www.ecologie.gouv.fr/pollution-lair-origines-situation-et-impacts")
+    st.markdown("Les gazs à effets de serres : https://jancovici.com/changement-climatique/gaz-a-effet-de-serre-et-cycle-du-carbone/quels-sont-les-gaz-a-effet-de-serre-quels-sont-leurs-contribution-a-leffet-de-serre/)")
+    st.markdown("Les Hydrocarbures : https://www.geo.fr/environnement/hydrocarbure-definition-classification-et-utilisation-193625")
+    st.markdown("Les polluants de l'air : https://www.ecologie.gouv.fr/pollution-lair-origines-situation-et-impacts")
 
     md_justify("Le Potentiel Global de Réchauffement (PGR) est une force radiative cumulée sur une durée (généralement 100 ans) d'un quantité de gaz donnée." \
     "Il permet de comparer l'impact de différents gaz à effet de serre en les ramenant à une même unité, le CO2 équivalent (CO2e)." \
