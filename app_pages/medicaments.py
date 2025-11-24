89965cc5933b3c3a9163c6f6be5d08072b44f0c6
FrenchSolex <le.cowboy.fringant@hotmail.fr>
2025-11-24 16:02:52 +0100

    streamlit PBI

--- app_pages/medicaments.py
+++ app_pages/medicaments.py
@@ -1,2 +1,23 @@
 import streamlit as st
 from pathlib import Path
+
+def md_justify(txt: str):
+    st.markdown(f"<div style='text-align: justify'>{txt}</div>", unsafe_allow_html=True)
+
+def render():
+    st.title("Dépenses des médicaments en ville en France")
+    st.markdown("---")
+
+    st.subheader("Contexte du projet")
+    md_justify()
+
+        # Chemins (depuis la racine du projet)
+    ROOT = Path(__file__).resolve().parents[1]
+    CLEAN = ROOT / "medicines" / "processed"
+
+    panorama_path = CLEAN.parent / "plot" / "Panorama.png"
+    col_l, col_c, col_r = st.columns([1, 2, 1])
+    with col_c:
+        if panorama_path.exists():
+            st.image(str(panorama_path), width=800)
+
