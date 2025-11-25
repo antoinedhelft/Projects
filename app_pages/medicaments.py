6f66d59e577028389d22d759dd43007e1d23151f
FrenchSolex <le.cowboy.fringant@hotmail.fr>
2025-11-25 11:20:30 +0100

    texte premier png

--- app_pages/medicaments.py
+++ app_pages/medicaments.py
@@ -5,19 +5,28 @@ def md_justify(txt: str):
     st.markdown(f"<div style='text-align: justify'>{txt}</div>", unsafe_allow_html=True)
 
 def render():
-    st.title("Coût de prise en charge des médicaments dispensés en ville en France")
+    st.title("Analyse des coûts et volumes des médicaments remboursés (France, 2021-2024)")
     st.markdown("---")
 
     st.subheader("Contexte du projet")
-    md_justify("En France, l'Assurance Maladie joue un rôle central dans la prise en charge des dépenses de santé. "
-               "Ce projet propose une analyse détaillée des coûts liés aux médicaments dispensés en ville sur la période 2021-2024.<br><br>"
+    md_justify("Ce projet Power BI vise à monitorer et analyser les dépenses de santé liées aux médicaments de ville." 
+               "L'objectif était d'identifier les tendances de consommation et les coûts associés afin d'optimiser la gestion des ressources de santé publique dans un contexte post-pandémique et inflationniste.<br><br>" 
                "Les données exploitées proviennent de la plateforme publique <b>data.gouv.fr</b>.<br><br>"
                "L'analyse s'appuie sur la classification ATC (Anatomique, Thérapeutique et Chimique) et explore les données à différents niveaux de granularité :<br>"
                "- Classe thérapeutique<br>"
                "- Classe pharmacologique<br>"
                "- Classe chimique<br>"
                "- Substance chimique<br><br>"
-               "Ce projet a été réalisé à l'aide de Power BI.")
+)
+    
+    st.subheader("Méthodologie & Visualisation :")
+    st.justify("- Analyse temporelle : Mise en parallèle du volume (boîtes) et de la valeur (montant remboursé) pour repérer les décrochages. On note une saisonnalité marquées avec des pics hivernaux et des creux estivaux.<br>" 
+    "- Indicateur coût unitaire : Focus spécifique sur l'évolution du prix moyen part boîte, révélant une tendance inflationniste.<br>"
+    "- KPIs macro : Vue d'ensemble sur la période (94 Mrd€ de dépenses pour 9.3 Mrd de boîtes). <br>")
+
+    st.subheader("Insights Clés :")
+    st.justify("1. un effet 'Ciseaux' : ALors que les volumes semblent suivre une saisonnalité stable, le montant remboursé montre des pics de plus en plus hauts, suggérant que la hausse des dépenses n'est pas uniquement due à une surconsommation.<br>"
+               "2. Inflation du coût moyen : L'insight majeur est l'augmentation continue du prix moyen de la boîte remboursée, passant de 9.27€ en 2021 à 11.31€ en 2024, soit une augmentation de +22% en 4 ans. Cela soulève une question : consommons-nous des médicaments plus chers, ou les médicaments existants coûtent-ils plus cher?<br>")
 
         # Chemins (depuis la racine du projet)
     ROOT = Path(__file__).resolve().parents[1]
