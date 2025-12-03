cae99ea9a7bf846ed3e28f2435cad1f63b97014e
FrenchSolex <le.cowboy.fringant@hotmail.fr>
2025-12-03 16:31:40 +0100

    corrections accueil streamlit

--- app_pages/accueil.py
+++ app_pages/accueil.py
@@ -10,8 +10,8 @@ def render():
         """
         Utilisez le menu de gauche pour naviguer entre les pages :
         - Médicaments : Analyses des coûts de prise en charge des médicaments en ville en France par la sécurité sociale à différents niveaux de classification ATC.
-        - Pollution VL : Analyses des émissions polluantes des Véhicules Légers (VL) et de leurs incidences sur les tendances d'achat chez les particuliers en France.
+        - Cryptomonnaies : Création d'un bot permettant la prédiction de la valeur des cryptomonnaies à court terme (4h) et des probabilités de hausse, baisse ou stagnation.
+        - Pollution Véhicules Légers : Analyses des émissions polluantes des Véhicules Légers (VL) et de leurs incidences sur les tendances d'achat chez les particuliers en France.
         """
-        #- Crypto : Création d'un bot permettant la prédiction de la valeur des cryptomonnaies à court terme (1h) et des probabilités de hausse, baisse ou stagnation.
         #- Trail : Suivi et analyse de mes performances en course à pied.   
     )
