0d7307e07901fbf73dcfefae1a7eb9d8802eec91
FrenchSolex <le.cowboy.fringant@hotmail.fr>
2025-12-30 18:26:05 +0100

    augmentation taille photo de profil

--- app_pages/accueil.py
+++ app_pages/accueil.py
@@ -38,8 +38,8 @@ def render():
         st.markdown(
             """
             <div style="display: flex; flex-direction: column; align-items: center;">
-                <img src="https://github.com/antoinedhelft.png" style="border-radius: 50%; width: 170px; height: 170px; object-fit: cover;">
-                <p style="font-size: 0.8rem; color: #6b7280; margin-top: 10px; text-align: center;">Antoine – Data engineer / Pharmacien</p>
+                <img src="https://github.com/antoinedhelft.png" style="border-radius: 50%; width: 200px; height: 200px; object-fit: cover;">
+                <p style="font-size: 0.8rem; color: #6b7280; margin-top: 20px; text-align: center;">Antoine – Data engineer / Pharmacien</p>
             </div>
             """,
             unsafe_allow_html=True
